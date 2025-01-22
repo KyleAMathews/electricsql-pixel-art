a10b12963674b0575326e335f0b52242a2377f5b
Kyle Mathews <mathews.kyle@gmail.com>
2025-01-22 16:43:17 -0700

    update for cloud changes

--- infra/create-db-and-add-to-electric.ts
+++ infra/create-db-and-add-to-electric.ts
@@ -4,16 +4,37 @@ export function createExampleDbAndAddtoElectric({ name }: { name: string }) {
   const NEON_ELECTRIC_EXAMPLES_ID = `bold-bush-75922852`;
   const project = neon.getProjectOutput({ id: NEON_ELECTRIC_EXAMPLES_ID });
 
-  const db = new neon.Database(`${name}-${$app.stage}`, {
+  const dbName = `${$app.name}-${$app.stage}`;
+
+  const { ownerName, dbName: resultingDbName } = createNeonDb({
     projectId: project.id,
     branchId: project.defaultBranchId,
-    name:
-      $app.stage === `Production`
-        ? `${name}-production`
-        : `${name}-${$app.stage}`,
-    ownerName: `neondb_owner`,
+    dbName,
   });
-  const { databaseUri, pooledDb } = getNeonDbUri(project, db);
+
+  const databaseUri = getNeonConnectionString({
+    project,
+    roleName: ownerName,
+    databaseName: resultingDbName,
+    pooled: false,
+  });
+  const pooledDb = getNeonConnectionString({
+    project,
+    roleName: ownerName,
+    databaseName: resultingDbName,
+    pooled: true,
+  });
+
+  // const db = new neon.Database(`${name}-${$app.stage}`, {
+  //   projectId: project.id,
+  //   branchId: project.defaultBranchId,
+  //   name:
+  //     $app.stage === `Production`
+  //       ? `${name}-production`
+  //       : `${name}-${$app.stage}`,
+  //   ownerName: `neondb_owner`,
+  // });
+  // const { databaseUri, pooledDb } = getNeonDbUri(project, db);
 
   const electricInfo = databaseUri.apply((uri) => addDatabaseToElectric(uri));
   electricInfo.apply(console.log);
@@ -28,7 +49,7 @@ export function createExampleDbAndAddtoElectric({ name }: { name: string }) {
   const databaseUriLink = new sst.Linkable(`databaseUriLink`, {
     properties: {
       url: databaseUri,
-      pooledUrl: pooledDb
+      pooledUrl: pooledDb,
     },
   });
 
@@ -54,7 +75,7 @@ function getNeonDbUri(
 async function addDatabaseToElectric(
   uri: string,
 ): Promise<{ id: string; token: string }> {
-  const adminApi = `https://admin-api-dev-production.electric-sql.com`;
+  const adminApi = `https://admin-api.electric-sql.cloud`;
 
   const result = await fetch(`${adminApi}/v1/sources`, {
     method: `PUT`,
@@ -62,7 +83,7 @@ async function addDatabaseToElectric(
     body: JSON.stringify({
       database_url: uri,
       region: `us-east-1`,
-      team_id: `d2c34c12-a9c3-4f89-9e9c-c234fd5c3f89`
+      team_id: `d2c34c12-a9c3-4f89-9e9c-c234fd5c3f89`,
     }),
   });
 
@@ -73,4 +94,102 @@ async function addDatabaseToElectric(
   }
 
   return await result.json();
+}
+
+function getNeonConnectionString({
+  project,
+  roleName,
+  databaseName,
+  pooled,
+}: {
+  project: $util.Output<neon.GetProjectResult>;
+  roleName: $util.Input<string>;
+  databaseName: $util.Input<string>;
+  pooled: boolean;
+}): $util.Output<string> {
+  const passwordOutput = neon.getBranchRolePasswordOutput({
+    projectId: project.id,
+    branchId: project.defaultBranchId,
+    roleName: roleName,
+  });
+
+  const endpoint = neon.getBranchEndpointsOutput({
+    projectId: project.id,
+    branchId: project.defaultBranchId,
+  });
+  const databaseHost = pooled
+    ? endpoint.endpoints?.apply((endpoints) =>
+        endpoints![0].host.replace(
+          endpoints![0].id,
+          endpoints![0].id + `-pooler`,
+        ),
+      )
+    : project.databaseHost;
+  return $interpolate`postgresql://${passwordOutput.roleName}:${passwordOutput.password}@${databaseHost}/${databaseName}?sslmode=require`;
+}
+
+/**
+ * Uses the [Neon API](https://neon.tech/docs/manage/databases) along with
+ * a Pulumi Command resource and `curl` to create and delete Neon databases.
+ */
+function createNeonDb({
+  projectId,
+  branchId,
+  dbName,
+}: {
+  projectId: $util.Input<string>;
+  branchId: $util.Input<string>;
+  dbName: $util.Input<string>;
+}): $util.Output<{
+  dbName: string;
+  ownerName: string;
+}> {
+  if (!process.env.NEON_API_KEY) {
+    throw new Error(`NEON_API_KEY is not set`);
+  }
+
+  const ownerName = `neondb_owner`;
+
+  const createCommand = `curl -f "https://console.neon.tech/api/v2/projects/$PROJECT_ID/branches/$BRANCH_ID/databases" \
+    -H 'Accept: application/json' \
+    -H "Authorization: Bearer $NEON_API_KEY" \
+    -H 'Content-Type: application/json' \
+    -d '{
+      "database": {
+        "name": "'$DATABASE_NAME'",
+        "owner_name": "${ownerName}"
+      }
+    }' 2>&1 \
+    && echo " SUCCESS" || echo " FAILURE - Response: $?"`;
+
+  const updateCommand = `echo "Cannot update Neon database with this provisioning method SUCCESS"`;
+
+  const deleteCommand = `curl -f -X 'DELETE' \
+    "https://console.neon.tech/api/v2/projects/$PROJECT_ID/branches/$BRANCH_ID/databases/$DATABASE_NAME" \
+    -H 'Accept: application/json' \
+    -H "Authorization: Bearer $NEON_API_KEY" 2>&1 \
+    && echo " SUCCESS" || echo " FAILURE - Response: $?"`;
+
+  const result = new command.local.Command(`neon-db-command:${dbName}`, {
+    create: createCommand,
+    update: updateCommand,
+    delete: deleteCommand,
+    environment: {
+      NEON_API_KEY: process.env.NEON_API_KEY,
+      PROJECT_ID: projectId,
+      BRANCH_ID: branchId,
+      DATABASE_NAME: dbName,
+    },
+  });
+  return $resolve([result.stdout, dbName]).apply(([stdout, dbName]) => {
+    if (stdout.endsWith(`SUCCESS`)) {
+      console.log(`Created Neon database ${dbName}`);
+      return {
+        dbName,
+        ownerName,
+      };
+    } else {
+      throw new Error(`Failed to create Neon database ${dbName}: ${stdout}`);
+    }
+  });
 }
--- sst.config.ts
+++ sst.config.ts
@@ -1,15 +1,19 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 import { execSync } from "child_process";
 import { createExampleDbAndAddtoElectric } from "./infra/create-db-and-add-to-electric";
-
 export default $config({
   app(input) {
     return {
       name: "electric-game",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
-      providers: { neon: "0.6.3" },
+      providers: {
+        neon: "0.6.3",
+        aws: {
+          profile: "marketing",
+        },
+        command: "1.0.1",
+      },
     };
   },
   async run() {
@@ -17,24 +21,19 @@ export default $config({
       const { electricInfo, databaseUri } = createExampleDbAndAddtoElectric({
         name: `electric-game`,
       });
-
       databaseUri.properties.url.apply(applyMigrations);
-
       const electricUrlLink = new sst.Linkable("ElectricUrl", {
         properties: {
           url: process.env.ELECTRIC_URL,
         },
       });
-
       // Add Cloudflare Worker
       const worker = new sst.cloudflare.Worker("electric-game-api", {
         handler: "./server/index.ts",
         url: true,
         link: [databaseUri, electricInfo, electricUrlLink],
       });
-
       const website = deploySite(electricInfo, worker);
-
       return {
         databaseUri: databaseUri.properties.url,
         pooled: databaseUri.properties.pooledUrl,
@@ -47,7 +46,6 @@ export default $config({
     }
   },
 });
-
 function applyMigrations(uri: string) {
   console.log(`apply migrations to `, uri);
   execSync(`npx pg-migrations apply --directory ./migrations`, {
@@ -57,14 +55,18 @@ function applyMigrations(uri: string) {
     },
   });
 }
-
 function deploySite(
-  electricInfo: sst.Linkable<{ source_id: string; token: string }>,
+  electricInfo: sst.Linkable<{
+    source_id: string;
+    token: string;
+  }>,
   worker: sst.cloudflare.Worker,
 ) {
   return new sst.aws.StaticSite("game-app", {
     domain: {
-      name: `pixel-art.examples${$app.stage === `production` ? `` : `-stage-${$app.stage}`}.electric-sql.com`,
+      name: `pixel-art.examples${
+        $app.stage === `production` ? `` : `-stage-${$app.stage}`
+      }.electric-sql.com`,
       dns: sst.cloudflare.dns(),
     },
     dev: {
@@ -77,7 +79,7 @@ function deploySite(
     environment: {
       VITE_ELECTRIC_TOKEN: electricInfo.properties.token,
       VITE_SOURCE_ID: electricInfo.properties.source_id,
-      VITE_ELECTRIC_URL: process.env.ELECTRIC_URL,
+      VITE_ELECTRIC_URL: `https://api.electric-sql.cloud`,
       VITE_API_URL: worker.url as unknown as string,
     },
   });
